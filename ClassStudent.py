d6e72a74c48e1c20775c08fb80effc0e1d242bf7
Artem Ksenofontov <tema339517@gmail.com>
2023-11-26 20:22:53 +0300

    Третий коммит

--- ClassStudent.py
+++ ClassStudent.py
@@ -22,12 +22,28 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def rate_lecture(self, lecturer, course, grade):
+        if course in self.courses_in_progress and isinstance(lecturer,
+                                                             Lecturer) and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Lecturer(Mentor):
     pass  # атрибуты или методы для Lecturer
 
 class Reviewer(Mentor):
-    pass  # атрибуты или методы для Reviewer
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -41,3 +57,12 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+cool_lecturer = Lecturer('Some', 'Lecturer')
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_lecture(cool_lecturer, 'Python', 10)
+best_student.rate_lecture(cool_lecturer, 'Python', 9)
+best_student.rate_lecture(cool_lecturer, 'Python', 8)
+
+print(cool_lecturer.grades)
